84f9d621682cb73856f00997bdb7fce0f2643b03
Christopher Stephen <cstephen1982@gmail.com>
2019-09-28 20:48:41 +1000

    Added extension class FireMonster

--- MonsterFighter/src/Monster.java
+++ MonsterFighter/src/Monster.java
@@ -1,50 +1,57 @@
 
 public class Monster {
 
+	private String name;
 	private double str;
 	private double intel;
 	private double dex;
 	private int hp;
-	private String name;
+	private int mp;
 	private int lvl;
 
-	public Monster(String name, double str, double intel, double dex, int hp, int lvl) {
+	public Monster(String name, double str, double intel, double dex, int hp, int mp, int lvl) {
+		this.name = name;
 		this.str = str;
 		this.intel = intel;
 		this.dex = dex;
 		this.hp = hp;
-		this.name = name;
+		this.mp = mp;
 		this.lvl = lvl;
 	}
-	
+
 	public String setName(String name) {
 		this.name = name;
 		return name;
 	}
-	
+
 	public double setStr(double strength) {
 		this.str = strength;
 		return strength;
 	}
-	
+
 	public double setInt(double intelligence) {
 		this.intel = intelligence;
 		return intelligence;
 	}
-	
+
 	public double setDex(double dexterity) {
 		this.dex = dexterity;
 		return dexterity;
 	}
-	
+
 	public int setHp(int health) {
 		this.hp = health;
 		return health;
 	}
 	
+	public int setMp(int mp) {
+		this.mp = mp;
+		return mp;
+	}
+
 	public int setLvl(int level) {
 		this.lvl = level;
 		return level;
 	}
-	
+
 }
--- MonsterFighter/src/MonsterFighter.java
+++ MonsterFighter/src/MonsterFighter.java
@@ -13,7 +13,8 @@ public class MonsterFighter {
 		String select;
 		boolean exit = false;
 		while (exit == false) {
-			print("\n\nEnter your selection!\n1 - Create a Monster\n2 - Fight Monsters\n3 - View a Monster\n4 - Exit\nWhat do you choose?: ");
+			print("\nEnter your selection!" + "\n1 - Create a Monster" + "\n2 - Fight Monsters" + "\n3 - View a Monster"
+					+ "\n4 - Exit\nWhat do you choose?: ");
 			select = sc.next();
 			switch (select) {
 			case "1":
@@ -26,34 +27,57 @@ public class MonsterFighter {
 			case "4":
 				exit = true;
 				break;
+			default:
+				print("\nIncorrect option\n");
+				break;
 			}
 		}
 	}
 
 	private static void createMonster() {
 		String name = "";
+		String select = "";
+		boolean menu = false;
 		double str = 0;
 		double intel = 0;
 		double dex = 0;
-		double hp = 0;
+		int hp = 100;
+		int mp = 100;
+		int lvl = 1;
 		if (count == 100) {
 			print("Monster roster full!");
 		} else {
-			print("Enter Monster name: ");
+			print("\nEnter Monster name: ");
 			name = sc.next();
 			print("\nRoll for STRENGTH!");
-			str = (Math.rint(Math.random() * ((20 - 1) + 1)) + 1);
+			str = (Math.rint(Math.random() * 20));
 			print("\nStrength is: " + str);
 			print("\nRoll for INTELLIGENCE!");
-			intel = (Math.rint(Math.random() * ((20 - 1) + 1)) + 1);
+			intel = (Math.rint(Math.random() * 20));
 			print("\nIntelligence is: " + intel);
 			print("\nRoll for DEXTERITY!");
-			dex = (Math.rint(Math.random() * ((20 - 1) + 1)) + 1);
+			dex = (Math.rint(Math.random() * 20));
 			print("\nDexterity is: " + dex);
-			hp = 100;
 			print("\nMonster HP is: " + hp);
+			print("\nMonster MP is: " + mp);
+			print("\nMonster Level is: " + lvl);
+			monsters[count] = new Monster(name, str, intel, dex, hp, mp, lvl);
 		}
-
+		print("\n\nSelect Monster type!" + "\n1 - Fire Monster, combines STRENGTH and DEXTERITY!"
+				+ "\n2 - Water Monster, combines DEXTERITY and INTELLIGENCE!"
+				+ "\n3 - Thunder Monster, combines INTELLIGENCE and STRENGTH!" + "\nEnter selection: ");
+		select = sc.next();
+		while (menu != true) {
+			switch (select) {
+			case "1":
+				print("\nFire Monster selected!");
+				monsters[count] = new FireMonster(name, str, intel, dex, hp, mp, lvl);
+				menu = true;
+				break;
+			default:
+				print("\nIncorrect option\n");
+			}
+		}
 	}
 
 	private static String print(String input) {
